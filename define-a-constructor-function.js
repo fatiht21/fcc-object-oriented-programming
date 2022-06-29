bef45ac366b4369fc789430a3b784b15267143d9
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-06-29 21:14:31 +0200

    fcss-object-oriented-programming

---
+++ define-a-constructor-function.js
@@ -0,0 +1,12 @@
+/*Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+
+Dog should have a name property set to a string.
+Dog should have a color property set to a string.
+Dog should have a numLegs property set to a number.
+*/
+
+function Dog() {
+  this.name = "Buddy";
+  this.color = "black";
+  this.numLegs = 4;
+}
